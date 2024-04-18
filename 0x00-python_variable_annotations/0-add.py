6e7d725cd6e029701a53a53e28d6707238972b9c
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 17:49:13 +0300

    Added doctest task 0

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
 
 """
-Write a type-annotated function add that takes a float a and a float b
-as arguments and returns their sum as a float.
+module that defines an add function
+add takes a float a and a float b
+add returns sum as a float
+
+>>> add(1.5, 2.5)
+4.0
 """
 
 
 def add(a: float, b: float) -> float:
     """
-    returns sum of two floats
+    returns sum of float and float b
     """
 
     return a + b
 
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
