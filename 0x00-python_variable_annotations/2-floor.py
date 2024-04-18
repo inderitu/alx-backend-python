6fd65c1afeb7215c7c3bb4f69b4eb5c4154cc50b
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 19:04:33 +0300

    task 4 done

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -5,6 +5,20 @@ module that defines a floor function
 
 >>> print(floor(3.14))
 3
+check n is of type float
+>>> floor(3)
+Traceback (most recent call last):
+    ...
+TypeError: n must be of type float
+
+>>> floor("string")
+Traceback (most recent call last):
+    ...
+TypeError: n must be of type float
+
+check for correct output
+>>> print(floor(3.14))
+3
 """
 import math
 
@@ -13,20 +27,7 @@ def floor(n: float) -> int:
     """
     float n as argument and returns the floor of the float.
 
-    check n is of type float
-    >>> floor(3)
-    Traceback (most recent call last):
-        ...
-    TypeError: n must be of type float
 
-    >>> floor("string")
-    Traceback (most recent call last):
-        ...
-    TypeError: n must be of type float
-
-    check for correct output
-    >>> print(floor(3.14))
-    3
     """
     if isinstance(n, float) is not True:
         raise TypeError("n must be of type float")
