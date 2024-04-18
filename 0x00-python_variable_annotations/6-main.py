e582d9456c524dea620ac6451a20d1ccdce7c6ec
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 21:45:09 +0300

    task 6 done

--- 0x00-python_variable_annotations/6-main.py
+++ 0x00-python_variable_annotations/6-main.py
@@ -1,5 +1,3 @@
-/**
-bob@dylan:~$ cat 6-main.py
 #!/usr/bin/env python3
 
 sum_mixed_list = __import__('6-sum_mixed_list').sum_mixed_list
@@ -9,9 +7,3 @@ mixed = [5, 4, 3.14, 666, 0.99]
 ans = sum_mixed_list(mixed)
 print(ans == sum(mixed))
 print("sum_mixed_list(mixed) returns {} which is a {}".format(ans, type(ans)))
-
-bob@dylan:~$ ./6-main.py
-{'mxd_lst': typing.List[typing.Union[int, float]], 'return': <class 'float'>}
-True
-sum_mixed_list(mixed) returns 679.13 which is a <class 'float'>
-*/
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,5 +1,53 @@
-/** 
- 
- 
-Write a type-annotated function sum_mixed_list which takes a list mxd_lst of integers and floats and returns their sum as a float. 
-*/
+#!/usr/bin/env python3
+
+"""
+module that creates function sum_mixed_list
+
+Write a type-annotated function sum_mixed_list
+which takes a list mxd_lst of integers andfloats
+returns their sum as a float.
+
+ensure input is list type
+>>> sum_mixed_list("abc")
+Traceback (most recent call last):
+    ...
+TypeError: mxd_lst must be a list
+
+ensure input list has only ints and floats
+>>> sum_mixed_list([1, 2.4, "d"])
+Traceback (most recent call last):
+    ...
+TypeError: mxd_lst must contain only int and floats
+
+ensure correct output
+>>> print(sum_mixed_list([1, 2.4]))
+3.4
+
+correct output type
+>>> isinstance(sum_mixed_list([1, 1]), float)
+True
+
+"""
+
+import typing
+
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    """
+    returns total sum of numbers in mxd_lst
+    """
+    total: float = 0.0
+
+    if isinstance(mxd_lst, list) is not True:
+        raise TypeError("mxd_lst must be a list")
+    for x in mxd_lst:
+        if isinstance(x, int | float) is not True:
+            raise TypeError("mxd_lst must contain only int and floats")
+        total += x
+
+    return total
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
