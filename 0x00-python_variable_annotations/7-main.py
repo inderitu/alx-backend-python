098e6ce9e340d834078c88b3edc22bc826753e27
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 22:34:48 +0300

    task 7 done

--- 0x00-python_variable_annotations/7-main.py
+++ 0x00-python_variable_annotations/7-main.py
@@ -1,5 +1,3 @@
-/**
-bob@dylan:~$ cat 7-main.py
 #!/usr/bin/env python3
 
 to_kv = __import__('7-to_kv').to_kv
@@ -7,9 +5,3 @@ to_kv = __import__('7-to_kv').to_kv
 print(to_kv.__annotations__)
 print(to_kv("eggs", 3))
 print(to_kv("school", 0.02))
-
-bob@dylan:~$ ./7-main.py
-{'k': <class 'str'>, 'v': typing.Union[int, float], 'return': typing.Tuple[str, float]}
-('eggs', 9)
-('school', 0.0004)
-*/
--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,5 +1,39 @@
-/** 
- 
- 
-Write a type-annotated function to_kv that takes a string k and an int OR float v as arguments and returns a tuple. The first element of the tuple is the string k. The second element is the square of the int/float v and should be annotated as a float. 
-*/
+#!/usr/bin/env python3
+
+"""
+Write a type-annotated function to_kv
+takes a string k and an int OR float v as arguments
+returns a tuple.
+The first element of the tuple is the string k.
+The second element is the square of the int/float v annotated as a float.
+
+ensure inputs are of correct types
+>>> to_kv(2, 2)
+Traceback (most recent call last):
+    ...
+TypeError: k must be a string
+
+>>> to_kv("test", "test")
+Traceback (most recent call last):
+    ...
+TypeError: v must be an int or float
+"""
+
+import typing
+
+
+def to_kv(k: str, v: typing.Union[int, float]) -> typing.Tuple[str, float]:
+    """
+    returns tuple (k, v *v)
+    """
+    if isinstance(k, str) is not True:
+        raise TypeError("k must be a string")
+    if isinstance(v, int | float) is not True:
+        raise TypeError("v must be an int or float")
+
+    return (k, v * v)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
