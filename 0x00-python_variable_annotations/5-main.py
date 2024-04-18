27ba4fb2be76ce309c9d25b01b90074946b9ae7d
Innocent-Bern <innocentbern22@gmail.com>
2024-04-18 19:34:25 +0300

    task 5 done

--- 0x00-python_variable_annotations/5-main.py
+++ 0x00-python_variable_annotations/5-main.py
@@ -1,5 +1,3 @@
-/**
-bob@dylan:~$ cat 5-main.py
 #!/usr/bin/env python3
 
 sum_list = __import__('5-sum_list').sum_list
@@ -8,10 +6,5 @@ floats = [3.14, 1.11, 2.22]
 floats_sum = sum_list(floats)
 print(floats_sum == sum(floats))
 print(sum_list.__annotations__)
-print("sum_list(floats) returns {} which is a {}".format(floats_sum, type(floats_sum)))
-
-bob@dylan:~$ ./5-main.py
-True
-{'input_list': typing.List[float], 'return': <class 'float'>}
-sum_list(floats) returns 6.470000000000001 which is a <class 'float'>
-*/
+print("sum_list(floats) returns {} which is a {}".format(
+    floats_sum, type(floats_sum)))
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,5 +1,55 @@
-/** 
- 
- 
-Write a type-annotated function sum_list which takes a list input_list of floats as argument and returns their sum as a float. 
-*/
+#!/usr/bin/env python3
+
+"""
+module that defines function sum_list
+Write a type-annotated function sum_list
+which takes a list input_list of floats as argument
+returns their sum as a float.
+
+ensure input_list is of type list
+>>> sum_list(3)
+Traceback (most recent call last):
+    ...
+TypeError: input_list must be a list
+
+>>> sum_list("string")
+Traceback (most recent call last):
+    ...
+TypeError: input_list must be a list
+
+ensure input_list contains only floats
+>>> sum_list([1.2, 3])
+Traceback (most recent call last):
+    ...
+TypeError: input_list must contain only floats
+
+check for correct output
+>>> print(sum_list([1.1, 2.1]))
+3.2
+
+check for float return type
+>>> isinstance(sum_list([1.1, 1.2]), float)
+True
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    returns sum of floats in input_list
+    """
+    total: float = 0.0
+
+    if isinstance(input_list, list) is not True:
+        raise TypeError("input_list must be a list")
+
+    for x in input_list:
+        if isinstance(x, float) is not True:
+            raise TypeError("input_list must contain only floats")
+        total += x
+
+    return total
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
